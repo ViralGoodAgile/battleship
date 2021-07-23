cf919321a526941a1abdddd4f6e51b2ea6cfa4f5
John Coleman <johncolemanagile@gmail.com>
2021-07-23 09:22:47 +0100

    Started palying around with different enemy positions

---
+++ Battleship.GameController/Contracts/Numbers.cs
@@ -0,0 +1,17 @@
+﻿namespace Battleship.GameController.Contracts
+{
+    /// <summary>
+    /// Numbers for the row
+    /// </summary>
+    public enum Numbers
+    {
+      One = 1,
+      Two = 2,
+      Three = 3,
+      Four=4,
+      Five = 5,
+      Six = 6,
+      Seven = 7,
+      Eight = 8
+    }
+}
---
+++ Battleship.GameController/Contracts/Opponents.cs
@@ -0,0 +1,19 @@
+﻿namespace Battleship.GameController.Contracts
+{
+   public enum Opponent
+    {
+        Arpad,
+        Ciprian,
+        Daniel,
+        David,
+        John,
+        Massimo,
+        Matthew,
+        Nich,
+        Peter,
+        Richard,
+        Sukhi,
+        Thomas,
+        Computer
+    }
+}
